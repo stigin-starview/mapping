dca7bd59280492cd3c42413bbe092d8e18f8be8f
stigin <stiginstaview@hotmail.com>
2021-12-09 10:41:23 +0530

    added marker and cleaned code

--- mapping.py
+++ mapping.py
@@ -1,3 +1,15 @@
 import folium
+
 map = folium.Map(location = [20.5937, 78.9629], zoom_start = 5, tiles = "Stamen Terrain")
+
+""" below is the standard way of doing this:
+
+map.save("IndianMap.html")  """
+
+# below is the clean way of doing.
+
+fg = folium.FeatureGroup(name = "Indian Map")
+fg.add_child(folium.Marker(location = [20.5937, 78.9629], popup = "This is an example of a marker", icon = folium.Icon(color = 'green')))
+map.add_child(fg)
+
 map.save("IndianMap.html")
