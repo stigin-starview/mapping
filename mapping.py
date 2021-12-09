449b307b5846777e98380758bc7c304ed8532070
stigin <stiginstaview@hotmail.com>
2021-12-09 15:58:19 +0530

    initializing the list adding method

--- mapping.py
+++ mapping.py
@@ -8,8 +8,11 @@ map.save("IndianMap.html")  """
 
 # below is the clean way of doing.
 
+#add for loop for automate the list
+
 fg = folium.FeatureGroup(name = "Indian Map")
 fg.add_child(folium.Marker(location = [20.5937, 78.9629], popup = "This is an example of a marker", icon = folium.Icon(color = 'green')))
 map.add_child(fg)
 
 map.save("IndianMap.html")
+    
