694214f6867d1d3dd4e74767ca0b12e857e0c022
stigin <stiginstaview@hotmail.com>
2021-12-14 21:05:41 +0530

    volcano information is added to the map with volcano name and type for loop is used to loop through the json file.

--- mapping.py
+++ mapping.py
@@ -4,14 +4,15 @@ import pandas
 map = folium.Map(location = [20.5937, 78.9629], zoom_start = 5, tiles = "Stamen Terrain")
 
 VolcanoData = pandas.read_json("resources/VolcanoList.json")
-# VolcanoName = dict(VolcanoData("name":"name", "type":"type"))
+VolcanoName = list(VolcanoData["name"])
+VolcanoType = list(VolcanoData["type"])
 VolcanoLat = list(VolcanoData["lat"])
 VolcanoLon = list(VolcanoData["lon"])
 
 fg = folium.FeatureGroup(name = "Indian Map")
 
-for lat, lon in zip(VolcanoLat, VolcanoLon):
-    fg.add_child(folium.Marker(location = [lat, lon], popup = "This is an example of a marker", icon = folium.Icon(color = 'red')))
+for name, type, lat, lon in zip(VolcanoName, VolcanoType, VolcanoLat, VolcanoLon):
+    fg.add_child(folium.Marker(location = [lat, lon], popup = f"name: {name}, type: {type}", icon = folium.Icon(color = 'green')))
 
 
 map.add_child(fg)
