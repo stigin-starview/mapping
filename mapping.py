b5008847417c3b40ff4b25690d4a5f98cfc96f77
stigin <stiginstaview@hotmail.com>
2021-12-14 19:50:50 +0530

    adding the volcano data around the world

--- mapping.py
+++ mapping.py
@@ -1,17 +1,19 @@
 import folium
+import pandas
 
 map = folium.Map(location = [20.5937, 78.9629], zoom_start = 5, tiles = "Stamen Terrain")
 
-""" below is the standard way of doing this:
-
-map.save("IndianMap.html")  """
-
-# below is the clean way of doing.
-
-#add for loop for automate the list
+VolcanoData = pandas.read_json("resources/VolcanoList.json")
+# VolcanoName = dict(VolcanoData("name":"name", "type":"type"))
+VolcanoLat = list(VolcanoData["lat"])
+VolcanoLon = list(VolcanoData["lon"])
 
 fg = folium.FeatureGroup(name = "Indian Map")
-fg.add_child(folium.Marker(location = [20.5937, 78.9629], popup = "This is an example of a marker", icon = folium.Icon(color = 'green')))
+
+for lat, lon in zip(VolcanoLat, VolcanoLon):
+    fg.add_child(folium.Marker(location = [lat, lon], popup = "This is an example of a marker", icon = folium.Icon(color = 'red')))
+
+
 map.add_child(fg)
 
 map.save("IndianMap.html")
