6cf003dd51f8aa6627c3a1cfa31e7b818f9588dc
stigin <stiginstaview@hotmail.com>
2021-12-15 13:02:35 +0530

    added more features to the volcano stylized the popup text added google search link for detailed information

--- mapping.py
+++ mapping.py
@@ -6,13 +6,21 @@ map = folium.Map(location = [20.5937, 78.9629], zoom_start = 5, tiles = "Stamen 
 VolcanoData = pandas.read_json("resources/VolcanoList.json")
 VolcanoName = list(VolcanoData["name"])
 VolcanoType = list(VolcanoData["type"])
+VolcanoElev = list(VolcanoData["elevation"])
 VolcanoLat = list(VolcanoData["lat"])
 VolcanoLon = list(VolcanoData["lon"])
 
+html = """<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank"> <h3> %s:</h3> </a>
+Type: %s <br>
+Height: %s m
+"""
+
 fg = folium.FeatureGroup(name = "Indian Map")
 
-for name, type, lat, lon in zip(VolcanoName, VolcanoType, VolcanoLat, VolcanoLon):
-    fg.add_child(folium.Marker(location = [lat, lon], popup = f"name: {name}, type: {type}", icon = folium.Icon(color = 'green')))
+for name, type, elev, lat, lon in zip(VolcanoName, VolcanoType, VolcanoElev, VolcanoLat, VolcanoLon):
+    iframe = folium.IFrame(html = html % (name + " volcano", name, type , elev), width = 200, height = 100)
+    # fg.add_child(folium.Marker(location = [lat, lon], popup = f"Name: {name}, Type: {type} , Elevation: {elev}m", icon = folium.Icon(color = 'green')))
+    fg.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(iframe), icon = folium.Icon(color = 'green')))
 
 
 map.add_child(fg)
