1b88e5c94059c53f26198cfc3023c60871813a81
stigin <stiginstaview@hotmail.com>
2021-12-16 19:08:28 +0530

    basic functions added step one of the project is completed

--- mapping.py
+++ mapping.py
@@ -4,6 +4,10 @@ import pandas
 map = folium.Map(location = [20.5937, 78.9629], zoom_start = 5, tiles = "Stamen Terrain")
 
 VolcanoData = pandas.read_json("resources/VolcanoList.json")
+PopulationData = open("resources/PopulationList.json", "r", encoding = "utf-8-sig").read()
+
+
+
 VolcanoName = list(VolcanoData["name"])
 VolcanoType = list(VolcanoData["type"])
 VolcanoElev = list(VolcanoData["elevation"])
@@ -11,6 +15,10 @@ VolcanoLat = list(VolcanoData["lat"])
 VolcanoLon = list(VolcanoData["lon"])
 
 # PopulationData = pandas.read_json("resources/PopulationList.json")
+
+# Volcones
+
+fgv = folium.FeatureGroup(name = "Volcanoes")
 
 html = """<a href="https://www.google.com/search?q=%%22%s%%22" target="_blank"> <h3> %s:</h3> </a>
 Type: %s <br>
@@ -25,23 +33,26 @@ def VolcanoColors(type):
     else:
         return "green"
 
-fg = folium.FeatureGroup(name = "Indian Map")
-
 for name, type, elev, lat, lon in zip(VolcanoName, VolcanoType, VolcanoElev, VolcanoLat, VolcanoLon):
     iframe = folium.IFrame(html = html % (name + " volcano", name, type , elev), width = 200, height = 100)
     # fg.add_child(folium.Marker(location = [lat, lon], popup = f"Name: {name}, Type: {type} , Elevation: {elev}m", icon = folium.Icon(color = 'green')))
     # fg.add_child(folium.Marker(location = [lat, lon], popup = folium.Popup(iframe), icon = folium.Icon(color = VolcanoColors(type))))
 
     #  To add the popup as circle use the code below
-    fg.add_child(folium.CircleMarker(location = [lat, lon], radius= 10, popup = folium.Popup(iframe), color = VolcanoColors(type), fill = True, fill_color = VolcanoColors(type)))
+    fgv.add_child(folium.CircleMarker(location = [lat, lon], radius= 10, popup = folium.Popup(iframe), color = VolcanoColors(type), fill = True, fill_color = VolcanoColors(type)))
 
 # Adding the population data informations
+fgp = folium.FeatureGroup(name = "Population")
+fgp.add_child(folium.GeoJson(data = PopulationData, style_function = lambda x: 
+                            {'fillColor':'green' if x['properties']['POP2005'] < 20000000 
+                            else 'orange' if x['properties']['POP2005'] < 50000000 else 'red'}))
 
-fg.add_child(folium.GeoJson(data = (open("resources/PopulationList.json", "r", encoding = "utf-8-sig").read())))
 
+map.add_child(fgv)
+map.add_child(fgp)
 
-map.add_child(fg)
-
+# Layer control fuvtion
+map.add_child(folium.LayerControl())
 map.save("IndianMap.html")
     
 
